a261811bc0d4cc96f494af078539ea696f587efd
Aayushi Singh <aayushi.singh067@gmail.com>
2024-07-14 08:52:33 +0530

    updated moving files to respective folders

--- app.js
+++ app.js
@@ -6,27 +6,35 @@ const { exit } = require('process')
 function createFolders() {
     fs.readdirSync(__dirname).forEach(file => {
         const fileExtension = path.extname(file)
+        const folderName = fileExtension.slice(1)
+        if(!fileExtension) return;
 
-        // const fileName = path.parse(file)
-        const strFile = fileExtension.toString()
+        if(!fs.existsSync(path.join(__dirname,folderName))){
+            fs.mkdirSync(path.join(__dirname, folderName))
+            console.log("Directory created:", folderName)
+        }
 
-        const fileName = strFile.slice(1, strFile.length)
-        
-        fs.mkdir(path.join(__dirname, fileName), (err) => {
-            if(err) {
-                exit
-            } console.log('directory created')
+        let newpath = path.join(__dirname, folderName, file)
+        let oldpath = path.join(__dirname, file)
+
+        fs.rename(oldpath, newpath, (err) => {
+            if (err) {
+                console.error("Error moving file:", err)
+                return
+            }
+            console.log("File moved:", file)
         })
     })
 }
-
 createFolders()
 
-function currentFiles() {
+
+function moveFiles() {
     console.log("\nCurrent files in directory:");
     fs.readdirSync(__dirname).forEach(file => {
         console.log(file)
     })
 }
+moveFiles()
 
-currentFiles()
+
